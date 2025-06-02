75e704864d48e94d4241af63e4658d3fb35377d2
Gabriel Salles <gs.amaral20@gmail.com>
2025-06-02 22:03:04 +0100

    adicionando exercicio_02.py da aula_01

---
+++ aula_01/exercicio_02.py
@@ -0,0 +1,8 @@
+# Crie um programa que o usuário digita 2 valores e retorna a soma deles
+
+#print(int(input("Digite o primeiro valor: ")) + int(input("Digite o segundo valor: ")))
+
+valor_01 = int(input("Digite o primeiro valor: "))
+valor_02 = int(input("Digite o segundo valor: "))
+soma = valor_01 + valor_02
+print(soma)
